5a0413494f230dc79b9924d0bdc11a1c1b9a8480
rlefko <ryan@avoca.ai>
2025-10-13 18:05:53 -0400

    Transform TTMoveHistory from global value to piece-to indexed table
    Changed TTMoveHistory from single StatsEntry to Stats indexed by
    [piece][to], enabling position-specific TT move quality tracking.
    
    This is the first step toward multi-ply TTMoveHistory extension.
    
    Updates:
    - history.h: Changed type to Stats[PIECE_NB][SQUARE_NB]
    - search.cpp: Updated initialization, queries, and updates
    
    Bench: 2183983

--- src/history.h
+++ src/history.h
@@ -163,7 +163,8 @@ struct CorrHistTypedef<NonPawn> {
 template<CorrHistType T>
 using CorrectionHistory = typename Detail::CorrHistTypedef<T>::type;
 
-using TTMoveHistory = StatsEntry<std::int16_t, 8192>;
+// TTMoveHistory tracks TT move quality indexed by [piece][to]
+using TTMoveHistory = Stats<std::int16_t, 8192, PIECE_NB, SQUARE_NB>;
 
 }  // namespace Stockfish
 
